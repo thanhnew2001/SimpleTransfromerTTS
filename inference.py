baac9c07d5195f2f15fcbd65056692d7e912fa69
thanhnew2001 <thanh.hispvietnam@gmail.com>
2024-03-20 03:00:50 +0700

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,38 @@
+import IPython
+import torch
+
+from model import TransformerTTS
+from melspecs import inverse_mel_spec_to_wav
+from write_mp3 import write_mp3
+
+
+train_saved_path = "train_SimpleTransfromerTTS.pt"
+state = torch.load(train_saved_path)
+model = TransformerTTS().cuda()
+model.load_state_dict(state["model"])
+
+text = "Hello, World."
+name_file = "hello_world.mp3"
+
+
+postnet_mel, gate = model.inference(
+  text_to_seq(text).unsqueeze(0).cuda(),
+  gate_threshold=1e-5,
+  with_tqdm = False
+)
+
+audio = inverse_mel_spec_to_wav(postnet_mel.detach()[0].T)
+
+plt.plot(
+    torch.sigmoid(gate[0, :]).detach().cpu().numpy()
+)
+
+write_mp3(
+    audio.detach().cpu().numpy(),
+    name_file
+)
+
+IPython.display.Audio(
+    audio.detach().cpu().numpy(),
+    rate=hp.sr
+)
